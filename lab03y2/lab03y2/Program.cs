5f6d37fa795b936da4c67c09a9cf1aea58295c57
ArslanPars <35731432+ArslanPars@users.noreply.github.com>
2018-10-11 11:52:38 +0300

    Update Program.cs

--- lab03y2/lab03y2/Program.cs
+++ lab03y2/lab03y2/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,72 +6,71 @@ using System.Threading.Tasks;
 
 namespace lab03y2
 {
+    public delegate double FuncDelegate(double x);
     class Program
     {
         static int Menu()
         {
             Console.WriteLine("Обчислення інтегралу методом:");
-            Console.WriteLine("1. Центральних прямокутників");
-            Console.WriteLine("2. Лівих прямокутників");
-            Console.WriteLine("3. Правих прямокутників");
+            Console.WriteLine("0. Центральних прямокутників");
+            Console.WriteLine("1. Лівих прямокутників");
+            Console.WriteLine("2. Правих прямокутників");
             Console.WriteLine("0. Вихід");
             var res = Console.ReadLine();
             Console.Clear();
             return Convert.ToInt32(res);
         }
-        static double Function(double x)
+
+        public static double QuadCentral(double a, double b, double n, FuncDelegate func)
+        {
+            Console.WriteLine("Метод центральних прямокутників:");
+            double h = (b - a) / n;
+            double q = 0;
+            for (double x = a; x < b; x += h)
+            {
+                q += func(x + h / 2);
+            }
+            return q * h;
+        }
+
+        public static double QuadLeft(double a, double b, double n, FuncDelegate func)
+        {
+            Console.WriteLine("Метод лівих прямокутників:");
+            double h = (b - a) / n;
+            double y1;
+            double x1;
+            double Intgrl = 0;
+            for (int i = 0; i < n; i++)
+            {
+                x1 = a + i * h;
+                y1 = func(x1);
+                Intgrl += y1 * h;
+            }
+            return Intgrl;
+        }
+
+         public static double QuadRight(double a, double b, double n, FuncDelegate func)
+        {
+            Console.WriteLine("Метод правих прямокутників:");
+            double h = (b - a) / n;
+            double y1;
+            double x1;
+            double Intgrl = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                x1 = a + i * h;
+                y1 = func(x1);
+                Intgrl += y1 * h;
+            }
+            return Intgrl;
+        }
+
+        delegate double QuadDelegate(double a, double b, double n, FuncDelegate func);
+        static double F(double x)
         {
             return Math.Pow(Math.Cos((2 * x + 2 * (5 * x) + 9) / 0.666), 3) + Math.Pow(5 * x, 7);
         }
-        static void M1(double a, double b, double n)
-        {
-            Console.WriteLine("Метод центральних прямокутників:");
-            double dx = (b - a) / n;
-            double y1, y2;
-            double x1, x2;
-            double Intgrl = 0;
-            for (int i = 0; i < n; i++)
-            {
-                x1 = a + i * dx;
-                x2 = x1 + dx;
-                y1 = Function(x1);
-                y2 = Function(x2);
-                Intgrl += (y1 + y2) / 2 * dx;
-            }
-            Console.WriteLine("Iнтеграл функцiї на вiдрiзку [{0}, {1}] становить {2:0.0000}", a, b, Intgrl);
 
-        }
-        static void M2(double a, double b, double n)
-        {
-            Console.WriteLine("Метод лівих прямокутників:");
-            double dx = (b - a) / n;
-            double y1;
-            double x1;
-            double Intgrl = 0;
-            for (int i = 0; i < n; i++)
-            {
-                x1 = a + i * dx;
-                y1 = Function(x1);
-                Intgrl += y1 * dx;
-            }
-            Console.WriteLine("Iнтеграл функцiї на вiдрiзку [{0}, {1}] становить {2:0.000000}", a, b, Intgrl);
-        }
-
-        static void M3(double a, double b, double n)
-        {
-            Console.WriteLine("Метод правих прямокутників:");
-            double dx = (b - a) / n;
-            double y1;
-            double x1;
-            double Intgrl = 0;
-            for (int i = 1; i < n; i++)
-            {
-                x1 = a + i * dx;
-                y1 = Function(x1);
-                Intgrl += y1 * dx;
-            }
-            Console.WriteLine("Iнтеграл функцiї на вiдрiзку [{0}, {1}] становить {2:0.0000}", a, b, Intgrl);
-        }
         static void Main(string[] args)
         {
             Console.Write("Введiть початок вiдрiзку iнтегрування a: ");
@@ -83,28 +82,13 @@ namespace lab03y2
             Console.Write("Введiть кiлькiсть дiлянок n: ");
             string sn = Console.ReadLine();
             double n = double.Parse(sn);
-            bool o = true;
-            while (o)
+            QuadDelegate[] menu = { QuadCentral, QuadLeft, QuadRight };
+            while (true)
             {
-                switch (Menu())
-                {
-                    case 1:
-                        M1(a, b, n);
-                        Console.ReadLine();
-                        break;
-                    case 2:
-                        M2(a, b, n);
-                        Console.ReadLine();
-                        break;
-                    case 3:
-                        M3(a, b, n);
-                        Console.ReadLine();
-                        break;
-                    case 0:
-                        o = false;
-                        Console.Write("До побачення!\n");
-                        break;
-                }
+                QuadDelegate q = menu[Menu()];
+                double r = q(a, b, n, F);
+                Console.WriteLine("Integral: [{0}, {1}] = {2}", a, b, r);
+                Console.WriteLine();
             }
         }
     }
