2b3ef4e03e4d6642ff4e16babc3c960027daddbe
ArslanPars <35731432+ArslanPars@users.noreply.github.com>
2018-11-22 12:07:14 +0200

    Add files via upload

---
+++ TestLab7/TestLab7/fMain.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestLab7
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void lab07y2_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void e(object sender, KeyPressEventArgs e)
+        {
+            Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(tbX1.Text) || (String.IsNullOrEmpty(tbX2.Text)))
+            {
+                tbY.Text = "Не введено даних!";
+                return;
+            }
+            double x1 = double.Parse(tbX1.Text);
+            double x2 = double.Parse(tbX2.Text);
+            double y = (Math.Pow (Math.Sin(x1), 3) + 45 + x2) / (2 * (x1 * x1 * x1 * x1) + 4 * x2);
+            tbY.Text = y.ToString("0.######");
+        }
+
+        private void btnClear_ClientSizeChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbX1.Text = string.Empty;
+            tbX2.Text = string.Empty;
+            tbY.Text = string.Empty;
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
