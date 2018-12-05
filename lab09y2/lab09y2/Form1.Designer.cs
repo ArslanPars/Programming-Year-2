2b94359f6af96e36352797c8425688eab22fe6c9
ArslanPars <35731432+ArslanPars@users.noreply.github.com>
2018-12-05 22:42:20 +0200

    Add files via upload

---
+++ lab09y2/lab09y2/Form1.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace lab09y2
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.X1min = new System.Windows.Forms.Label();
+            this.X1max = new System.Windows.Forms.Label();
+            this.dX1 = new System.Windows.Forms.Label();
+            this.X2min = new System.Windows.Forms.Label();
+            this.X2max = new System.Windows.Forms.Label();
+            this.dX2 = new System.Windows.Forms.Label();
+            this.gv = new System.Windows.Forms.DataGridView();
+            this.tbx1min = new System.Windows.Forms.TextBox();
+            this.tbdx2 = new System.Windows.Forms.TextBox();
+            this.tbdx1 = new System.Windows.Forms.TextBox();
+            this.tbx2max = new System.Windows.Forms.TextBox();
+            this.tbx2min = new System.Windows.Forms.TextBox();
+            this.tbx1max = new System.Windows.Forms.TextBox();
+            this.btnCalc = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // X1min
+            // 
+            this.X1min.AutoSize = true;
+            this.X1min.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X1min.Location = new System.Drawing.Point(24, 29);
+            this.X1min.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.X1min.Name = "X1min";
+            this.X1min.Size = new System.Drawing.Size(57, 20);
+            this.X1min.TabIndex = 0;
+            this.X1min.Text = "X1min";
+            // 
+            // X1max
+            // 
+            this.X1max.AutoSize = true;
+            this.X1max.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X1max.Location = new System.Drawing.Point(193, 29);
+            this.X1max.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.X1max.Name = "X1max";
+            this.X1max.Size = new System.Drawing.Size(57, 20);
+            this.X1max.TabIndex = 1;
+            this.X1max.Text = "X1max";
+            // 
+            // dX1
+            // 
+            this.dX1.AutoSize = true;
+            this.dX1.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dX1.Location = new System.Drawing.Point(363, 29);
+            this.dX1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.dX1.Name = "dX1";
+            this.dX1.Size = new System.Drawing.Size(39, 20);
+            this.dX1.TabIndex = 2;
+            this.dX1.Text = "dX1";
+            // 
+            // X2min
+            // 
+            this.X2min.AutoSize = true;
+            this.X2min.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X2min.Location = new System.Drawing.Point(31, 86);
+            this.X2min.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.X2min.Name = "X2min";
+            this.X2min.Size = new System.Drawing.Size(57, 20);
+            this.X2min.TabIndex = 3;
+            this.X2min.Text = "X2min";
+            // 
+            // X2max
+            // 
+            this.X2max.AutoSize = true;
+            this.X2max.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.X2max.Location = new System.Drawing.Point(200, 86);
+            this.X2max.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.X2max.Name = "X2max";
+            this.X2max.Size = new System.Drawing.Size(57, 20);
+            this.X2max.TabIndex = 4;
+            this.X2max.Text = "X2max";
+            // 
+            // dX2
+            // 
+            this.dX2.AutoSize = true;
+            this.dX2.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dX2.Location = new System.Drawing.Point(363, 86);
+            this.dX2.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.dX2.Name = "dX2";
+            this.dX2.Size = new System.Drawing.Size(39, 20);
+            this.dX2.TabIndex = 5;
+            this.dX2.Text = "dX2";
+            // 
+            // gv
+            // 
+            this.gv.BackgroundColor = System.Drawing.Color.SteelBlue;
+            this.gv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gv.Location = new System.Drawing.Point(28, 129);
+            this.gv.Name = "gv";
+            this.gv.ReadOnly = true;
+            this.gv.RowHeadersWidth = 100;
+            this.gv.Size = new System.Drawing.Size(489, 254);
+            this.gv.TabIndex = 6;
+            // 
+            // tbx1min
+            // 
+            this.tbx1min.Location = new System.Drawing.Point(89, 26);
+            this.tbx1min.MaxLength = 13;
+            this.tbx1min.Name = "tbx1min";
+            this.tbx1min.Size = new System.Drawing.Size(96, 28);
+            this.tbx1min.TabIndex = 7;
+            this.tbx1min.TextChanged += new System.EventHandler(this.tbx1min_TextChanged);
+            // 
+            // tbdx2
+            // 
+            this.tbdx2.Location = new System.Drawing.Point(421, 83);
+            this.tbdx2.MaxLength = 13;
+            this.tbdx2.Name = "tbdx2";
+            this.tbdx2.Size = new System.Drawing.Size(96, 28);
+            this.tbdx2.TabIndex = 8;
+            // 
+            // tbdx1
+            // 
+            this.tbdx1.Location = new System.Drawing.Point(421, 26);
+            this.tbdx1.MaxLength = 13;
+            this.tbdx1.Name = "tbdx1";
+            this.tbdx1.Size = new System.Drawing.Size(96, 28);
+            this.tbdx1.TabIndex = 9;
+            // 
+            // tbx2max
+            // 
+            this.tbx2max.Location = new System.Drawing.Point(258, 83);
+            this.tbx2max.MaxLength = 13;
+            this.tbx2max.Name = "tbx2max";
+            this.tbx2max.Size = new System.Drawing.Size(96, 28);
+            this.tbx2max.TabIndex = 10;
+            // 
+            // tbx2min
+            // 
+            this.tbx2min.Location = new System.Drawing.Point(89, 83);
+            this.tbx2min.MaxLength = 13;
+            this.tbx2min.Name = "tbx2min";
+            this.tbx2min.Size = new System.Drawing.Size(96, 28);
+            this.tbx2min.TabIndex = 11;
+            // 
+            // tbx1max
+            // 
+            this.tbx1max.Location = new System.Drawing.Point(258, 26);
+            this.tbx1max.MaxLength = 13;
+            this.tbx1max.Name = "tbx1max";
+            this.tbx1max.Size = new System.Drawing.Size(96, 28);
+            this.tbx1max.TabIndex = 12;
+            // 
+            // btnCalc
+            // 
+            this.btnCalc.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCalc.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btnCalc.Location = new System.Drawing.Point(558, 129);
+            this.btnCalc.Name = "btnCalc";
+            this.btnCalc.Size = new System.Drawing.Size(138, 34);
+            this.btnCalc.TabIndex = 13;
+            this.btnCalc.Text = "Calculet";
+            this.btnCalc.UseVisualStyleBackColor = true;
+            this.btnCalc.Click += new System.EventHandler(this.btnCalc_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnClear.Location = new System.Drawing.Point(558, 169);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(138, 34);
+            this.btnClear.TabIndex = 14;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExit.Location = new System.Drawing.Point(558, 349);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(138, 34);
+            this.btnExit.TabIndex = 15;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(734, 395);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnCalc);
+            this.Controls.Add(this.tbx1max);
+            this.Controls.Add(this.tbx2min);
+            this.Controls.Add(this.tbx2max);
+            this.Controls.Add(this.tbdx1);
+            this.Controls.Add(this.tbdx2);
+            this.Controls.Add(this.tbx1min);
+            this.Controls.Add(this.gv);
+            this.Controls.Add(this.dX2);
+            this.Controls.Add(this.X2max);
+            this.Controls.Add(this.X2min);
+            this.Controls.Add(this.dX1);
+            this.Controls.Add(this.X1max);
+            this.Controls.Add(this.X1min);
+            this.Font = new System.Drawing.Font("Harlow Solid Italic", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Margin = new System.Windows.Forms.Padding(5);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab09";
+            ((System.ComponentModel.ISupportInitialize)(this.gv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label X1min;
+        private System.Windows.Forms.Label X1max;
+        private System.Windows.Forms.Label dX1;
+        private System.Windows.Forms.Label X2min;
+        private System.Windows.Forms.Label X2max;
+        private System.Windows.Forms.Label dX2;
+        private System.Windows.Forms.DataGridView gv;
+        private System.Windows.Forms.TextBox tbx1min;
+        private System.Windows.Forms.TextBox tbdx2;
+        private System.Windows.Forms.TextBox tbdx1;
+        private System.Windows.Forms.TextBox tbx2max;
+        private System.Windows.Forms.TextBox tbx2min;
+        private System.Windows.Forms.TextBox tbx1max;
+        private System.Windows.Forms.Button btnCalc;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnExit;
+    }
+}
+
---
+++ lab09y2/lab09y2/Form1.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab09y2
+{
+    public partial class fMain : Form
+    {
+        public fMain()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalc_Click(object sender, EventArgs e)
+        {
+            double x1min = 0;
+            double x2min = 0;
+            double dx1 = 0;
+            double x1max = 0;
+            double x2max = 0;
+            double dx2 = 0;
+            try
+            {
+                x1min = double.Parse(tbx1min.Text);
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Erroe X1min {0}", ex.Message);
+                MessageBox.Show(err, "Converison error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbx1min.Text = tbx1min.Text.Remove(tbx1min.Text.Length - tbx1min.Text.Length);
+            }
+            try
+            {
+                x2min = double.Parse(tbx2min.Text);
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Erroe x2mix {0}", ex.Message);
+                MessageBox.Show(err, "Converison error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbx2min.Text = tbx2min.Text.Remove(tbx2min.Text.Length - tbx2min.Text.Length);
+            }
+            try
+            {
+                dx1 = double.Parse(tbdx1.Text);
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Erroe dX1 {0}", ex.Message);
+                MessageBox.Show(err, "Converison error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbdx1.Text = tbdx1.Text.Remove(tbdx1.Text.Length - tbdx1.Text.Length);
+            }
+            try
+            {
+                x1max = double.Parse(tbx1max.Text);
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Erroe X1max {0}", ex.Message);
+                MessageBox.Show(err, "Converison error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbx1max.Text = tbx1max.Text.Remove(tbx1max.Text.Length - 12);
+            }
+            try
+            {
+                x2max = double.Parse(tbx2max.Text);
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Erroe X2max {0}", ex.Message);
+                MessageBox.Show(err, "Converison error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbx2max.Text = tbx2max.Text.Remove(tbx2max.Text.Length - tbx2max.Text.Length);
+            }
+            try
+            {
+                dx2 = double.Parse(tbdx2.Text);
+            }
+            catch (Exception ex)
+            {
+                string err = string.Format("Erroe dX2 {0}", ex.Message);
+                MessageBox.Show(err, "Converison error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                tbdx2.Text = tbdx2.Text.Remove(tbdx2.Text.Length - tbdx2.Text.Length);
+            }
+
+            gv.ColumnCount = (int)Math.Truncate((x2max - x2min) / dx2) + 1;
+            gv.RowCount = (int)Math.Truncate((x1max - x1min) / dx1) + 1;
+            for (int i = 0; i < gv.RowCount; i++)
+                gv.Rows[i].HeaderCell.Value = (x1min + i * dx1).ToString("0.000");
+            gv.RowHeadersWidth = 80;
+            for (int i = 0; i < gv.ColumnCount; i++)
+            {
+                gv.Columns[i].HeaderCell.Value = (x2min + i * dx2).ToString("0.000");
+                gv.Columns[i].Width = 60;
+            }
+            int cl, rw;
+            double x1, x2, y;
+            rw = 0;
+            x1 = x1min;
+            while (x1 <= x1max)
+            {
+                x2 = x2min;
+                cl = 0;
+                while (x2 <= x2max)
+                {
+                    y = (Math.Pow(Math.Sin(x1), 3) + 45 + x2) / 2 * Math.Pow(x1, 4) + 4 * x2;
+                    gv.Rows[rw].Cells[cl].Value = y.ToString("#.####");
+                    x2 += dx2; cl++;
+                }
+                x1 += dx1;
+                rw++;
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            tbx1min.Text = "";
+            tbx1max.Text = "";
+            tbx2min.Text = "";
+            tbx2max.Text = "";
+            tbdx1.Text = "";
+            tbdx2.Text = "";
+            gv.Rows.Clear();
+            for (int Cl = 0; Cl < gv.ColumnCount; Cl++)
+                gv.Columns[Cl].HeaderCell.Value = "";
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Закрити програму?", "Вихід з програми", MessageBoxButtons.OKCancel, MessageBoxIcon.Question) == DialogResult.OK)
+                Application.Exit();
+        }
+
+        private void tbx1min_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
