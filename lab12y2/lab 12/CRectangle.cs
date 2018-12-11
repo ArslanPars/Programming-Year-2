1eff2ab80a1a6261ea5c20c8b08c2b3073a0a519
ArslanPars <35731432+ArslanPars@users.noreply.github.com>
2018-12-11 22:50:22 +0200

    Add files via upload

---
+++ lab12y2/lab 12/CRectangle.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace lab_12
+{
+    class CRectangle : CFigure
+    {
+        private int _Side;
+
+        public int Side
+        {
+            get
+            {
+                return _Side;
+            }
+            set
+            {
+                _Side = value >= 200 ? 200 : value;
+                _Side = value <= 5 ? 5 : value;
+            }
+        }
+
+        public CRectangle(Graphics graphics, int X, int Y, int Side, 
+            int SideB)
+        {
+            this.graphics = graphics;
+            this.X = X;
+            this.Y = Y;
+            this.Side = Side;
+        }
+
+        protected override void Draw(Pen pen)
+        {
+            Rectangle rectangle = new Rectangle(X - Side / 2,  Y - Side / 2, Side, Side);
+            graphics.DrawRectangle(pen, rectangle);
+        }
+
+        override public void Expand(int dX)
+        {
+            Hide();
+            Side = Side + dX;
+            Show();
+        }
+
+        override public void Collapse(int dX)
+        {
+            Hide();
+            Side = Side - dX;
+            Show();
+        }
+
+    }
+}
---
+++ lab12y2/lab 12/fMain.Designer.cs
@@ -0,0 +1,304 @@
+﻿namespace lab_12
+{
+    partial class fMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnMain = new System.Windows.Forms.Panel();
+            this.pnTools = new System.Windows.Forms.Panel();
+            this.btnLeftFar = new System.Windows.Forms.Button();
+            this.btnLeft = new System.Windows.Forms.Button();
+            this.btnRightFar = new System.Windows.Forms.Button();
+            this.btnRight = new System.Windows.Forms.Button();
+            this.btnDownFar = new System.Windows.Forms.Button();
+            this.btnDown = new System.Windows.Forms.Button();
+            this.btnUpFar = new System.Windows.Forms.Button();
+            this.btnUp = new System.Windows.Forms.Button();
+            this.btnCollapse = new System.Windows.Forms.Button();
+            this.btnExpand = new System.Windows.Forms.Button();
+            this.btnHide = new System.Windows.Forms.Button();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.btnCreateNew = new System.Windows.Forms.Button();
+            this.cbFigureType = new System.Windows.Forms.ComboBox();
+            this.cbCircle = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnTools.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnMain
+            // 
+            this.pnMain.BackColor = System.Drawing.Color.White;
+            this.pnMain.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnMain.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnMain.Location = new System.Drawing.Point(0, 0);
+            this.pnMain.Margin = new System.Windows.Forms.Padding(4);
+            this.pnMain.Name = "pnMain";
+            this.pnMain.Size = new System.Drawing.Size(796, 475);
+            this.pnMain.TabIndex = 0;
+            // 
+            // pnTools
+            // 
+            this.pnTools.Controls.Add(this.btnLeftFar);
+            this.pnTools.Controls.Add(this.btnLeft);
+            this.pnTools.Controls.Add(this.btnRightFar);
+            this.pnTools.Controls.Add(this.btnRight);
+            this.pnTools.Controls.Add(this.btnDownFar);
+            this.pnTools.Controls.Add(this.btnDown);
+            this.pnTools.Controls.Add(this.btnUpFar);
+            this.pnTools.Controls.Add(this.btnUp);
+            this.pnTools.Controls.Add(this.btnCollapse);
+            this.pnTools.Controls.Add(this.btnExpand);
+            this.pnTools.Controls.Add(this.btnHide);
+            this.pnTools.Controls.Add(this.btnShow);
+            this.pnTools.Controls.Add(this.btnCreateNew);
+            this.pnTools.Controls.Add(this.cbFigureType);
+            this.pnTools.Controls.Add(this.cbCircle);
+            this.pnTools.Controls.Add(this.label1);
+            this.pnTools.Dock = System.Windows.Forms.DockStyle.Right;
+            this.pnTools.Location = new System.Drawing.Point(453, 0);
+            this.pnTools.Margin = new System.Windows.Forms.Padding(4);
+            this.pnTools.Name = "pnTools";
+            this.pnTools.Size = new System.Drawing.Size(343, 475);
+            this.pnTools.TabIndex = 1;
+            // 
+            // btnLeftFar
+            // 
+            this.btnLeftFar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLeftFar.Image = global::lab_12.Properties.Resources.left_left;
+            this.btnLeftFar.Location = new System.Drawing.Point(47, 296);
+            this.btnLeftFar.Name = "btnLeftFar";
+            this.btnLeftFar.Size = new System.Drawing.Size(41, 71);
+            this.btnLeftFar.TabIndex = 16;
+            this.btnLeftFar.UseVisualStyleBackColor = true;
+            this.btnLeftFar.Click += new System.EventHandler(this.btnLeftFar_Click);
+            // 
+            // btnLeft
+            // 
+            this.btnLeft.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnLeft.Image = global::lab_12.Properties.Resources.left;
+            this.btnLeft.Location = new System.Drawing.Point(94, 296);
+            this.btnLeft.Name = "btnLeft";
+            this.btnLeft.Size = new System.Drawing.Size(35, 71);
+            this.btnLeft.TabIndex = 15;
+            this.btnLeft.UseVisualStyleBackColor = true;
+            this.btnLeft.Click += new System.EventHandler(this.btnLeft_Click);
+            // 
+            // btnRightFar
+            // 
+            this.btnRightFar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRightFar.Image = global::lab_12.Properties.Resources.right_right;
+            this.btnRightFar.Location = new System.Drawing.Point(261, 296);
+            this.btnRightFar.Name = "btnRightFar";
+            this.btnRightFar.Size = new System.Drawing.Size(41, 71);
+            this.btnRightFar.TabIndex = 14;
+            this.btnRightFar.UseVisualStyleBackColor = true;
+            this.btnRightFar.Click += new System.EventHandler(this.btnRightFar_Click);
+            // 
+            // btnRight
+            // 
+            this.btnRight.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnRight.Image = global::lab_12.Properties.Resources.right;
+            this.btnRight.Location = new System.Drawing.Point(220, 296);
+            this.btnRight.Name = "btnRight";
+            this.btnRight.Size = new System.Drawing.Size(35, 71);
+            this.btnRight.TabIndex = 13;
+            this.btnRight.UseVisualStyleBackColor = true;
+            this.btnRight.Click += new System.EventHandler(this.btnRight_Click);
+            // 
+            // btnDownFar
+            // 
+            this.btnDownFar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDownFar.Image = global::lab_12.Properties.Resources.down_down;
+            this.btnDownFar.Location = new System.Drawing.Point(134, 412);
+            this.btnDownFar.Name = "btnDownFar";
+            this.btnDownFar.Size = new System.Drawing.Size(79, 38);
+            this.btnDownFar.TabIndex = 12;
+            this.btnDownFar.UseVisualStyleBackColor = true;
+            this.btnDownFar.Click += new System.EventHandler(this.btnDownFar_Click);
+            // 
+            // btnDown
+            // 
+            this.btnDown.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnDown.Image = global::lab_12.Properties.Resources.down;
+            this.btnDown.Location = new System.Drawing.Point(135, 372);
+            this.btnDown.Name = "btnDown";
+            this.btnDown.Size = new System.Drawing.Size(79, 32);
+            this.btnDown.TabIndex = 11;
+            this.btnDown.UseVisualStyleBackColor = true;
+            this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+            // 
+            // btnUpFar
+            // 
+            this.btnUpFar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUpFar.Image = global::lab_12.Properties.Resources.up_up;
+            this.btnUpFar.Location = new System.Drawing.Point(134, 213);
+            this.btnUpFar.Name = "btnUpFar";
+            this.btnUpFar.Size = new System.Drawing.Size(79, 38);
+            this.btnUpFar.TabIndex = 10;
+            this.btnUpFar.UseVisualStyleBackColor = true;
+            this.btnUpFar.Click += new System.EventHandler(this.btnUpFar_Click);
+            // 
+            // btnUp
+            // 
+            this.btnUp.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnUp.Image = global::lab_12.Properties.Resources.up;
+            this.btnUp.Location = new System.Drawing.Point(134, 257);
+            this.btnUp.Name = "btnUp";
+            this.btnUp.Size = new System.Drawing.Size(79, 32);
+            this.btnUp.TabIndex = 9;
+            this.btnUp.UseVisualStyleBackColor = true;
+            this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+            // 
+            // btnCollapse
+            // 
+            this.btnCollapse.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnCollapse.Image = global::lab_12.Properties.Resources.collapse;
+            this.btnCollapse.Location = new System.Drawing.Point(135, 335);
+            this.btnCollapse.Name = "btnCollapse";
+            this.btnCollapse.Size = new System.Drawing.Size(79, 32);
+            this.btnCollapse.TabIndex = 8;
+            this.btnCollapse.UseVisualStyleBackColor = true;
+            this.btnCollapse.Click += new System.EventHandler(this.btnCollapse_Click);
+            // 
+            // btnExpand
+            // 
+            this.btnExpand.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnExpand.Image = global::lab_12.Properties.Resources.expand;
+            this.btnExpand.Location = new System.Drawing.Point(135, 296);
+            this.btnExpand.Name = "btnExpand";
+            this.btnExpand.Size = new System.Drawing.Size(79, 32);
+            this.btnExpand.TabIndex = 7;
+            this.btnExpand.UseVisualStyleBackColor = true;
+            this.btnExpand.Click += new System.EventHandler(this.btnExpand_Click);
+            // 
+            // btnHide
+            // 
+            this.btnHide.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnHide.Location = new System.Drawing.Point(43, 126);
+            this.btnHide.Name = "btnHide";
+            this.btnHide.Size = new System.Drawing.Size(253, 29);
+            this.btnHide.TabIndex = 5;
+            this.btnHide.Text = "Hide object";
+            this.btnHide.UseVisualStyleBackColor = true;
+            this.btnHide.Click += new System.EventHandler(this.btnHide_Click);
+            // 
+            // btnShow
+            // 
+            this.btnShow.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnShow.Location = new System.Drawing.Point(43, 161);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(253, 29);
+            this.btnShow.TabIndex = 4;
+            this.btnShow.Text = "Show object";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // btnCreateNew
+            // 
+            this.btnCreateNew.Location = new System.Drawing.Point(43, 90);
+            this.btnCreateNew.Name = "btnCreateNew";
+            this.btnCreateNew.Size = new System.Drawing.Size(133, 29);
+            this.btnCreateNew.TabIndex = 3;
+            this.btnCreateNew.Text = "Create new object";
+            this.btnCreateNew.UseVisualStyleBackColor = true;
+            this.btnCreateNew.Click += new System.EventHandler(this.btnCreateNew_Click);
+            // 
+            // cbFigureType
+            // 
+            this.cbFigureType.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbFigureType.FormattingEnabled = true;
+            this.cbFigureType.Items.AddRange(new object[] {
+            "Circle",
+            "Rectengle",
+            "Triangle"});
+            this.cbFigureType.Location = new System.Drawing.Point(182, 92);
+            this.cbFigureType.Name = "cbFigureType";
+            this.cbFigureType.Size = new System.Drawing.Size(114, 27);
+            this.cbFigureType.TabIndex = 2;
+            this.cbFigureType.SelectedIndexChanged += new System.EventHandler(this.cbFigureType_SelectedIndexChanged);
+            // 
+            // cbCircle
+            // 
+            this.cbCircle.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cbCircle.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCircle.FormattingEnabled = true;
+            this.cbCircle.Location = new System.Drawing.Point(21, 42);
+            this.cbCircle.Name = "cbCircle";
+            this.cbCircle.Size = new System.Drawing.Size(301, 27);
+            this.cbCircle.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(110, 18);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "List of objects";
+            // 
+            // fMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 475);
+            this.Controls.Add(this.pnTools);
+            this.Controls.Add(this.pnMain);
+            this.Font = new System.Drawing.Font("Harlow Solid Italic", 11.25F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "fMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Lab12";
+            this.pnTools.ResumeLayout(false);
+            this.pnTools.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnMain;
+        private System.Windows.Forms.Panel pnTools;
+        private System.Windows.Forms.Button btnHide;
+        private System.Windows.Forms.Button btnShow;
+        private System.Windows.Forms.Button btnCreateNew;
+        private System.Windows.Forms.ComboBox cbFigureType;
+        private System.Windows.Forms.ComboBox cbCircle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLeftFar;
+        private System.Windows.Forms.Button btnLeft;
+        private System.Windows.Forms.Button btnRightFar;
+        private System.Windows.Forms.Button btnRight;
+        private System.Windows.Forms.Button btnDownFar;
+        private System.Windows.Forms.Button btnDown;
+        private System.Windows.Forms.Button btnUpFar;
+        private System.Windows.Forms.Button btnUp;
+        private System.Windows.Forms.Button btnCollapse;
+        private System.Windows.Forms.Button btnExpand;
+    }
+}
+
